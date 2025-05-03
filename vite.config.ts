7fe49af6a0916364e4acdc542c4bd8460c74a504
manjaycoder <manjay.verma.coder@gmail.com>
2025-05-03 14:11:51 +0530

    nsns

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Manjay-webdev/',
   build: {
     outDir: 'dist',
     sourcemap: false,
